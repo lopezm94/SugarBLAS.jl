a90abc64713557ab97c6f4334cb62a36da54faec
lopezm94 <juanlopezm94@gmail.com>
2017-12-07 17:07:43 +0100

    Fix Metada filter

--- src/SugarBLAS.jl
+++ src/SugarBLAS.jl
@@ -110,10 +110,17 @@ Sugar for if-then-else expression. Beautiful for one liners.
 """
 macro case(expr::Expr)
     (expr.head == :block) || error("@case statement must be followed by `begin ... end`")
-    lines = filter(expr -> typeof(expr) != LineNumberNode, expr.args)
+    lines = []
+    lines = filter(line::Expr -> !is_metadata(line), expr.args)
     exec = construct_case_statement(lines)
     esc(Meta.parse(exec))
 end
+
+"""
+Filter metadata lines that often appear in Julia expressions
+"""
+is_metadata(line::Expr) = line.head == :line #Needed for versions older than v0.7-DEV
+is_metadata(expr::LineNumberNode) = true
 
 ###############
 # BLAS macros #
